bc3178091555ae31c4d4221c60690f26071197ff
benivgy <140189587+benivgy@users.noreply.github.com>
2024-05-12 21:10:24 +0300

    Update NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -3,26 +3,18 @@ from tensorflow import keras
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import numpy as np
 import tensorflow as tf  # Import TensorFlow module
 
 
-#clean the csv file:
-def clean():
-    #Removing the last column
-
-    print("reading")
-    df=pd.read_csv('nl_data.csv')
-    print("finished")
-    column_to_delete = df.columns[26]
-    df=df.drop(column_to_delete, axis=1)
-    df.to_csv('nl_data_cleaned.csv', index=False) #no index column
-
 def ann():
     print("loading")
-    df = pd.read_csv('mini.csv')
+    df = pd.read_csv('nl_data_cleaned.csv')
     print("loaded")
     X = df.drop(df.columns[25], axis=1).to_numpy()
     y = df.iloc[:, 25].to_numpy()
+    print("finished")
+    print("training")
 
     # Set train and test- 20% for test and 80% for train
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size =0.25 )
@@ -32,13 +24,13 @@ def ann():
     model = keras.Sequential(
         [
             layers.Dense(256, activation='relu', input_dim=25),
-            layers.Dense(128, activation='relu'),
+            layers.Dense(128, activation='relu'),#
             layers.Dense(64, activation='relu'),
-            layers.Dense(32, activation='relu'),
+            layers.Dense(32, activation='relu'),#
             layers.Dense(16, activation='relu'),
-            layers.Dense(8, activation='relu'),
+            layers.Dense(8, activation='relu'),#
             layers.Dense(4, activation='relu'),
-            layers.Dense(2, activation='relu'),
+            layers.Dense(2, activation='relu'),#
             layers.Dense(1, activation='linear'),
         ]
     )
@@ -68,6 +60,10 @@ def ann():
 
     model.save('my_model.h5')
 
+def grade(boardTOpredict): #Returns the grade from the neural network
+    data_array = np.array(list(map(int, boardTOpredict.split(','))))
+    model = keras.models.load_model('my_model.h5')
+    return model.predict(data_array.reshape(1, -1))[0][0]
 
 if __name__=="__main__":
-    ann()
+    print(grade('0,0,2,1,0,0,1,0,0,0,0,0,0,2,0,0,0,0,1,0,2,0,1,0,2'))
